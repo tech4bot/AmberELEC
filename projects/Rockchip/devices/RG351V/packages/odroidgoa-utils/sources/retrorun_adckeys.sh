961c9e8a4488f1739f4382faba66d07f829f28be
tech4bot <hackaround4fun@gmail.com>
2024-02-16 22:52:54 +0100

    extend adckeys script to ports and n64

--- projects/Rockchip/devices/RG351V/packages/odroidgoa-utils/sources/retrorun_adckeys.sh
+++ projects/Rockchip/devices/RG351V/packages/odroidgoa-utils/sources/retrorun_adckeys.sh
@@ -4,7 +4,7 @@ DEVICE_FILE="/dev/input/by-path/platform-rg351-keys-event"
 
 while true; do
     evtest "$DEVICE_FILE" | while read -r line; do
-    if pgrep -f "/usr/bin/retrorun.sh" >/dev/null; then
+    if pgrep -f "/usr/bin/retrorun.sh" >/dev/null || pgrep -f "/roms/ports" >/dev/null || pgrep -f "/usr/local/bin/mupen64plus" >/dev/null ; then
         if [[ $line == *"KEY_ESC"* ]]; then
             if [[ $line == *"value 1"* ]]; then
                 /usr/bin/adckeys.py startselect
