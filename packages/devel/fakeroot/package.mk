2a71e16cb5ede2e7a97a745792f9e3eb3b4a9b36
tech4bot <hackaround4fun@gmail.com>
2025-04-13 17:10:47 +0200

    small fixes

--- packages/devel/fakeroot/package.mk
+++ packages/devel/fakeroot/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fakeroot"
-PKG_VERSION="1.36.2"
-PKG_SHA256="92ee28cd75ef17a178bb06d9b9f57fb54b068b6a72d4710cccfe8453701c734f"
+PKG_VERSION="1.37.1.1"
+PKG_SHA256="86b0b75bf319ca42e525c098675b6ed10a06b76e69ec9ccf20ef5e03883b3a14"
 PKG_LICENSE="GPL3"
 PKG_SITE="https://tracker.debian.org/pkg/fakeroot"
 PKG_URL="http://ftp.debian.org/debian/pool/main/f/fakeroot/${PKG_NAME}_${PKG_VERSION}.orig.tar.gz"
